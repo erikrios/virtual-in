3783b20d71cdb8fdf026d8638898417ab6067793
erikrios <erikriosetiawan49@gmail.com>
2019-11-09 05:36:50 +0700

    Add model class and fragment

---
+++ app/src/main/java/com/erikriosetiawan/virtualin/models/Umkm.kt
@@ -0,0 +1,9 @@
+package com.erikriosetiawan.virtualin.models
+
+data class Umkm(
+    var name: String? = null,
+    var founder: String? = null,
+    var rating: Double? = null,
+    var icon: Int? = null,
+    var about: String? = null
+)
